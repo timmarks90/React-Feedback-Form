c3929a83a4ee156b1cb9298a632bc57bdee3bb50
tim.marks <timmarks90@gmail.com>
2022-01-01 07:40:58 -0800

    updated card jsx

--- src/components/shared/Card.jsx
+++ src/components/shared/Card.jsx
@@ -1,9 +1,4 @@
 function Card({ children, reverse }) {
-    // return (
-    //     <div className={`card ${reverse && 'reverse'}`}>
-    //         {children}
-    //     </div>
-    // )
     return (
         <div className='card' style={{
                 backgroundColor: reverse ? 'rgba(0,0,0,.4' : '#fff', 
